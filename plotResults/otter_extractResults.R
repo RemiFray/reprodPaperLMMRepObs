a6a95710e5aeb7f3852ec503d82244fab7ea8b61
Remi FRAYSSE <fray.r7@gmail.com>
2025-05-07 10:26:01 +0200

    Update full extract script for otter

--- plotResults/otter_extractResults.R
+++ plotResults/otter_extractResults.R
@@ -48,16 +48,29 @@ for(i in 1:3){
 
 
 yoshi <- read.csv("./results/otter_Yoshizaki_Results.csv") %>% 
-  select(Model, year, N = N_mean, N_sd, p1 = p1_mean, p2 = p2_mean,
-         p3 = p3_mean, p4 = p4_mean, p5 = p5_mean) %>% 
+  select(Model, year, N = N_mean, N_2.5, N_97.5, N_sd, N_Rhat, N_n.eff,
+         p1_mean, p1_2.5, p1_97.5, 
+         p2_mean, p2_2.5, p2_97.5, 
+         p3_mean, p3_2.5, p3_97.5, 
+         p4_mean, p4_2.5, p4_97.5, 
+         p5_mean, p5_2.5, p5_97.5) %>% 
   mutate(alpha = rep(NA, 6), alpha_sd = rep(NA, 6), nbErr = rep(NA, 6))
 LMM2 <- read.csv("./results/otter_LMM2_Results.csv") %>% 
-  select(Model, year, N = N_mean, N_sd, alpha = a_mean, alpha_sd = a_sd, 
-         nbErr = nbErrTot_mean, p1 = l1_mean, p2 = l2_mean,
-         p3 = l3_mean, p4 = l4_mean, p5 = l5_mean)
+  select(Model, year, N = N_mean, N_2.5, N_97.5, N_sd, N_Rhat, N_n.eff,
+         alpha = a_mean, alpha_sd = a_sd, 
+         nbErr = nbErrTot_mean, 
+         p1_mean = l1_mean, p1_2.5 = l1_2.5, p1_97.5 = l1_97.5, 
+         p2_mean = l2_mean, p2_2.5 = l2_2.5, p2_97.5 = l2_97.5, 
+         p3_mean = l3_mean, p3_2.5 = l3_2.5, p3_97.5 = l3_97.5, 
+         p4_mean = l4_mean, p4_2.5 = l4_2.5, p4_97.5 = l4_97.5, 
+         p5_mean = l5_mean, p5_2.5 = l5_2.5, p5_97.5 = l5_97.5)
 Mt <- read.csv("./results/otter_Mt_Results.csv") %>% 
-  select(Model, year, N = N_mean, N_sd, p1 = p1_mean, p2 = p2_mean,
-         p3 = p3_mean, p4 = p4_mean, p5 = p5_mean) %>% 
+  select(Model, year, N = N_mean, N_2.5, N_97.5, N_sd, N_Rhat, N_n.eff,
+         p1_mean, p1_2.5, p1_97.5, 
+         p2_mean, p2_2.5, p2_97.5, 
+         p3_mean, p3_2.5, p3_97.5, 
+         p4_mean, p4_2.5, p4_97.5, 
+         p5_mean, p5_2.5, p5_97.5) %>% 
   mutate(alpha = rep(NA, 6), alpha_sd = rep(NA, 6),  nbErr = rep(NA, 6))
 
 AllModels <- rbind(yoshi, LMM2, Mt)
